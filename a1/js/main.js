fc306982996cea6fe5c74cbf1a632a440a45dfe6
Tay Yang Shun <tay.yang.shun@gmail.com>
2014-09-06 21:35:42 +0800

    Save into logs

--- a1/js/main.js
+++ a1/js/main.js
@@ -50,8 +50,19 @@ autoComPasteApp.controller('InstructionsController', function ($scope, $location
 });
 
 autoComPasteApp.controller('TrialController', function ($scope, $location, $http) {
-  
+  var logs = [[ 'participant_id',
+                'technique',
+                'granularity',
+                'open_windows',
+                'trial_no',
+                'stimuli',
+                'user_response',
+                'trial_time',
+                'accuracy'
+                ]];
+
   var pid = 'P1';
+  $scope.rested = false;
 
   $http({method: 'GET', url: '/js/participants.json'}).
   success(function(data, status, headers, config) {
@@ -69,13 +80,35 @@ autoComPasteApp.controller('TrialController', function ($scope, $location, $http
       if ($scope.currentTrialNum >= 3) {
         $scope.currentTrialNum = $scope.currentTrialNum%3;
         $scope.currentBlockNum++;
+        if ($scope.currentBlockNum == 9 && !$scope.rested) {
+          $scope.currentBlockNum = 8;
+          $scope.currentTrialNum = 2;
+          $scope.triggerBreak();
+          return;
+        }
       }
       $scope.currentBlock = $scope.blocks[$scope.currentBlockNum];
 
       var endTime = (new Date()).getTime();
-      console.log('Timing:', endTime - startTime);
+      var trialTime = endTime - startTime;
+      console.log('Trial Time:', trialTime);
+
+      var stimuli = $('#stimuli').text();
+      var response = $('#TextEditor_textArea').val();
+      var row = [pid,
+                $scope.currentBlock.technique,
+                $scope.currentBlock.granularity,
+                ($scope.currentTrialNum+1) * 2,
+                $scope.currentBlockNum * 3 + $scope.currentTrialNum,
+                stimuli,
+                response,
+                trialTime,
+                stimuli == response ? 1 : 0
+                ];
+
+      logs.push(row);
+
       startTime = (new Date()).getTime();
-      
       runTrial($scope.currentTrialNum, $scope.currentBlock.stimuli[$scope.currentTrialNum], isAutoComPaste);
     }
   }).
@@ -84,7 +117,28 @@ autoComPasteApp.controller('TrialController', function ($scope, $location, $http
     // or server returns response with an error status.
   });
 
-  
+  var interval;
+
+  $scope.triggerBreak = function () {
+    $('#Root').html('');
+    $scope.break = true;
+    interval = setInterval(function () {
+      $scope.time -= 1;
+      $scope.$apply();
+      if ($scope.time <= 0) {
+        $scope.endBreak();
+      }
+    }, 1000);
+  }
+
+  $scope.endBreak = function () {
+    clearInterval(interval);
+    $scope.rested = true;
+    $scope.$apply()
+    $scope.nextTrial();
+  }
+
+  $scope.time = 60;
 });
 
 autoComPasteApp.controller('PostController', function ($scope, $location) {
